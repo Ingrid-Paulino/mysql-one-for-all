a7b2e12ec8a92f59971f0e00e09e42024228c21d
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2022-01-06 08:50:28 -0300

    C5 - Add requisito 2

--- desafio2.sql
+++ desafio2.sql
@@ -1,1 +1,14 @@
-SELECT * FROM plano;
+CREATE TABLE total_cancoes_artistas_albuns(
+  total_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  cancoes INT NOT NULL,
+  artistas INT NOT NULL,
+  albuns INT NOT NULL
+) ENGINE=InnoDB;
+
+
+INSERT INTO total_cancoes_artistas_albuns(
+  cancoes,
+  artistas,
+  albuns)
+VALUES
+  (40, 6, 10);
