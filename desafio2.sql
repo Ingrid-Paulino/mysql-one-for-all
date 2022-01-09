b8f296ba5e3158a6a340862dc191e4e3d11db296
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2022-01-09 10:19:46 -0300

    C18 - tentando passar requisito 2 pera 3° vez

--- desafio2.sql
+++ desafio2.sql
@@ -1,7 +1,15 @@
+-- esta dando um erro
+-- SELECT 
+-- 	COUNT(distinct cancoes.cancoes) AS cancoes,
+--     COUNT(DISTINCT alb.artista_id) AS artistas,
+--     COUNT(DISTINCT alb.album_id) AS albuns
+-- FROM SpotifyClone.cancoes
+-- INNER JOIN SpotifyClone.album AS alb
+--   ON cancoes.cancoes_id = alb.album_id;
+
+
 SELECT
-	COUNT(C.cancoes) AS cancoes,
-  COUNT(DISTINCT alb.artista_id) AS artistas,
-  COUNT(DISTINCT alb.album_id) AS albuns
-FROM SpotifyClone.cancoes AS C
-INNER JOIN SpotifyClone.album AS alb
-  ON C.cancoes_id = alb.album_id;
+	COUNT(cancoes) AS cancoes,
+    COUNT(DISTINCT artista_id) AS artistas,
+    COUNT(DISTINCT album_id) AS albuns
+FROM SpotifyClone.cancoes;
--- desafio3.sql
+++
@@ -1,3 +0,0 @@
-SELECT nome_usuario AS usuario, IF(condicao_id = 1, 'Usuário ativo', 'Usuário inativo') AS condicao_usuario
-FROM SpotifyClone.usuario
-ORDER BY nome_usuario ASC;
