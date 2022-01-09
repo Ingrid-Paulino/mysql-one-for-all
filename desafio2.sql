d306379dc95780e0fa71520ac9757a0d3025ae8f
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2022-01-09 07:32:04 -0300

    C16 - tentando passar requisito 2

--- desafio2.sql
+++ desafio2.sql
@@ -1,13 +1,5 @@
--- SELECT 
---   COUNT(cancoes),
---   COUNT(DISTINCT(artista_id)) AS artistas,
---   COUNT(DISTINCT(album_id)) AS albuns
--- FROM SpotifyClone.cancoes;
-
 SELECT
-  COUNT(cancao_id) AS `cancoes`,
-  COUNT(DISTINCT(artista_id)) AS `artistas`,
-  COUNT(DISTINCT(cancoes.album_id)) AS `albuns`
-FROM SpotifyClone.cancoes
-  INNER JOIN SpotifyClone.albuns
-  ON cancoes.album_id = albuns.album_id;
+	COUNT(cancoes),
+    COUNT(DISTINCT artista_id) AS artistas,
+    COUNT(DISTINCT album_id) AS albuns
+FROM SpotifyClone.cancoes;
