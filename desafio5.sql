2908dea95d570056cd23b758b6bd008a574d1e3b
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2022-01-11 19:26:44 -0300

    C26 - add req 5

---
+++ desafio5.sql
@@ -0,0 +1,8 @@
+SELECT 
+	C.cancoes,
+    COUNT(H.cancoes_id) AS reproducoes
+FROM SpotifyClone.cancoes AS C
+INNER JOIN SpotifyClone.historico_reproducao AS H
+	ON C.cancoes_id = H.cancoes_id
+GROUP BY C.cancoes
+ORDER BY reproducoes DESC, C.cancoes LIMIT 2;
