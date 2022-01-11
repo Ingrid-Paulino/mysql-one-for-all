f26b6ebf955270fd6285364fbef7b6ddba09a42b
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2022-01-11 11:01:25 -0300

    C20 - add req 3

---
+++ desafio3.sql
@@ -0,0 +1,12 @@
+SELECT 
+    U.nome_usuario AS usuario,
+    COUNT(H.usuario_id) AS qtde_musicas_ouvida,
+    ROUND(SUM(C.duracao_segundos) / 60,2) AS total_minutos
+FROM
+    SpotifyClone.usuario AS U
+INNER JOIN
+    SpotifyClone.historico_reproducao AS H ON U.usuario_id = H.usuario_id
+INNER JOIN SpotifyClone.cancoes AS C
+	ON C.cancoes_id = H.cancoes_id
+GROUP BY nome_usuario
+ORDER BY nome_usuario;
